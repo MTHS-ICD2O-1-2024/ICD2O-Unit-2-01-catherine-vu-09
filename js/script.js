5e46ad99dd8d170d6e47545d0d05759e1fdcecfe
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-03-06 17:26:17 +0000

    added style

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Catherine
+// Created on: March 6 2025
 // This file contains the JS functions for index.html
+
+/**
+ * Displays an alert with the message "Hello, World!"
+ */
+function myButtonClicked() {
+  document.getElementById("answer").innerHTML = "Hello, World!"
+}
